5002cb4499a4ae9e6b09f2c8eecda19d9561e83a
jwschroeder3 <schroedj@umich.edu>
2021-08-24 15:49:23 -0400

    back to poorly-written code.py for instruction purposes

--- code.py
+++ code.py
@@ -1,58 +1,16 @@
-'''Simple utilites for counting and reporting the fraction of positions
-in a sequence that belong to each base.
-'''
-
-def count_basie(seq):
-    '''Counts the number of occurances of each base in seq.
-
-    Args:
-    -----
-    seq : str
-        The sequence in which to count base occurances.
-
-    Returns:
-    --------
-    base_count_dict : dict
-        A dictionary, the keys of which are base identities,
-        the values of which are the number of occurences of the key base.
-    '''
-
-    base_count_dict = dict()
-    for base in seq:
-        if base not in base_count_dict:
-            base_count_dict[base] = 1
+def function1(a):
+    b = dict()
+    for c in a:
+        if c not in b:
+            b[c] = 1
         else:
-            base_count_dict[base] += 1
-    return base_count_dict
+            b[c] += 1
+    return b
 
-def print_base_fractions(count_dict):
-    '''Prints the proportion of positions having each base in
-    the input dictionary.
+def function2(a):
+    print('freqs')
+    total = float(sum([a[b] for b in a.keys()]))
+    for b in a.keys():
+        print(b + ':' + str(a[b]/total))
 
-    Args:
-    -----
-    count_dict : dict
-        Dictionary, the keys of which are base identities,
-        the values of which are the number of occurences of the base.
-
-    Returns:
-    --------
-    None
-
-    Prints:
-    -------
-    Fraction of bases counted in count_dict belonging to each base.
-    '''
-
-    print()
-    print('Fraction of bases of each identity in sequence')
-    print("----------------------------------------------")
-    total = float(sum([count_dict[base] for base in count_dict.keys()]))
-    for base in count_dict.keys():
-        base_frac = count_dict[base] / total
-        print(base + ': ' + str(base_frac))
-
-if __name__ == "__main__":
-    base_counts = count_basie('ATCTGACGCGCGCCGC')
-    print_base_fractions(base_counts)
-
+function2(function1('ATCTGACGCGCGCCGC'))
