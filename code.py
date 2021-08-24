bf64066fde7f1ec0116f17f7b20d18e62b8801f5
jwschroeder3 <schroedj@umich.edu>
2021-08-23 22:35:28 -0400

    added initial example of code that is a real slog.

---
+++ code.py
@@ -0,0 +1,16 @@
+def function1(a):
+    b = dict()
+    for c in a:
+        if c not in b:
+            b[c] = 1
+        else:
+            b[c] += 1
+    return b
+
+def function2(a):
+    print('freqs')
+    total = float(sum([a[b] for b in a.keys()]))
+    for b in a.keys():
+        print(b + ':' + str(a[b]/total))
+
+function2(function1('ATCTGACGCGCGCCGC'))
